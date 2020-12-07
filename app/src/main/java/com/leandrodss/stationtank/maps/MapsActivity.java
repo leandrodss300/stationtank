8fcaf68ca1ab4690053edd389ebffc47afe9d892
Aluno <Aluno>
2020-12-07 19:07:38 -0400

    MapsActivity 11/12

---
+++ app/src/main/java/com/leandrodss/stationtank/maps/MapsActivity.java
@@ -0,0 +1,16 @@
+package com.leandrodss.stationtank.maps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leandrodss.stationtank.R;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
