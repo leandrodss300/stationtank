d84383d3149f36959ada8bf26215953f1437992d
Aluno <Aluno>
2020-12-10 21:42:13 -0400

    Recuperando Inforamções do Local10/12

--- app/src/main/java/com/leandrodss/stationtank/activity/maps/MapsActivity.java
+++ app/src/main/java/com/leandrodss/stationtank/activity/maps/MapsActivity.java
@@ -9,6 +9,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,8 +25,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.leandrodss.stationtank.Permissoes;
 import com.leandrodss.stationtank.R;
+import com.leandrodss.stationtank.model.Posto;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -34,38 +47,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     };
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private DatabaseReference databaseReference;
 
+    private ArrayList<Posto> listaPostos = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps2);
-
+        databaseReference = FirebaseDatabase.getInstance().getReference("postos");
+        getData();
         //Validar permissões
         Permissoes.validarPermissoes(permissoes, this, 1);
-
-        //Objeto responsável por gerenciar a localização do usuário
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                Log.d("Localização","onLocationChanged: "+ location.toString());
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -87,6 +79,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //Objeto responsável por gerenciar a localização do usuário
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("Localização","onLocationChanged: "+ location.toString());
+
+                Double latitude = location.getLatitude();
+                Double longitude = location.getLongitude();
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    //List<Address> listaEndereco =  geocoder.getFromLocation(latitude, longitude, 1);
+                    String stringEndereco = "R. B, 2-212 - Armando Mendes, Manaus - AM, 69089-020";
+                    List<Address> listaEndereco =  geocoder.getFromLocationName(stringEndereco, 1);
+                    if(listaEndereco != null && listaEndereco.size() > 0){
+                        Address endereco = listaEndereco.get(0);
+
+                        Double lat = endereco.getLatitude();
+                        Double longi = endereco.getLongitude();
+
+                        //https://pt.stackoverflow.com/questions/89304/como-calcular-dist%C3%A2ncia-da-localiza%C3%A7%C3%A3o-do-usu%C3%A1rio-com-outro-endere%C3%A7o
+
+                        //mMap.clear();
+                        LatLng localUsuario = new LatLng(lat, longi);
+                        mMap.addMarker(new MarkerOptions().position(localUsuario).title("Meu local").icon(BitmapDescriptorFactory.fromResource(R.drawable.gasolinee)));
+                        //-3.088188, -59.946740
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localUsuario, 15));
+
+
+                        Log.d("local", "onLocationChanged: "+endereco.toString());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 0, 0, locationListener
+            );
+        }
+
         //Evento de click no mapa
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
@@ -104,11 +156,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         // Add a marker in Sydney and move the camera
-        LatLng manaus = new LatLng(-3.088406, -59.948216);
-        //-3.088406, -59.948216
-        mMap.addMarker(new MarkerOptions().position(manaus).title("Armando Mendes"));
-        //.icon(BitmapDescriptorFactory.fromResource(R.drawable.gasoline))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(manaus, 15));
     }
 
     @Override
@@ -154,4 +201,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+    public void getData(){
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for( DataSnapshot no_filho : snapshot.getChildren() ){
+                    Posto posto = no_filho.getValue(Posto.class);
+                    listaPostos.add(posto);
+                    Log.i("POSTO: ",posto.toString());
+                }
+
+                //desenhar os marcadores
+                for(Posto posto : listaPostos){
+                    LatLng localUsuario = new LatLng(posto.getLatitude(), posto.getLongitude());
+
+                    mMap.addMarker(new MarkerOptions().position(localUsuario).title(posto.getNome()).icon(BitmapDescriptorFactory.fromResource(R.drawable.gasolinee)));
+                    //avisando o adapter que os dados mudaram
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
 }
