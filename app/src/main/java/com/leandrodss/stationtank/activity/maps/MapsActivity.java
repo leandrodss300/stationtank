0b5b6b297dd7789bbff4d6f7fabed61f0c363295
Aluno <Aluno>
2020-12-11 21:47:19 -0400

    11/12 - 2

--- app/src/main/java/com/leandrodss/stationtank/activity/maps/MapsActivity.java
+++ app/src/main/java/com/leandrodss/stationtank/activity/maps/MapsActivity.java
@@ -135,7 +135,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER, 0, 0, locationListener
+                    LocationManager.GPS_PROVIDER, 20000, 0, locationListener
             );
         }
 
